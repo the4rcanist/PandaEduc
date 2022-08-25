dba6bbb332db9680e9406264925a5494150617c2
The4rcanist <109610368+the4rcanist@users.noreply.github.com>
2022-08-25 16:31:24 -0300

    Create funcaoprint.py

---
+++ Python/Módulo 1/Função Print()/funcaoprint.py
@@ -0,0 +1,5 @@
+#A função print() é uma das funções mais utilizadas e importantes na linguagem Python. O  uso mais comum dessa função é a exibição de mensagens na tela
+
+print('Bem vindo')
+
+#Para exibir o resultado na tela, basta passar a mensagem, desejada, como argumento da função envolvida por aspas (simples ou duplas) e delimitada por parenteses
